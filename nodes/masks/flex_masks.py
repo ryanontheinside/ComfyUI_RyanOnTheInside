c2f11894d623683202ef664b21cfabb1f43e58fd
ryanontheinstide <ryanfosdick87@gmail.com>
2024-08-29 22:05:22 -0400

    Squashed commit of the following:
    commit 40981b910fa8a8298f8aa1752f94577c682304f1
    Author: ryanontheinstide <ryanfosdick87@gmail.com>
    Date:   Thu Aug 29 17:41:51 2024 -0400
    
        bugfix
    
    commit 98b21aaa5051e825a42fa7e50ed2a7cc55311ffd
    Author: ryanontheinstide <ryanfosdick87@gmail.com>
    Date:   Thu Aug 29 12:24:22 2024 -0400
    
        feature modulation of particle systems
    
    commit d79ac2936c730ff4b1c4ee5e409809e469b02608
    Author: ryanontheinstide <ryanfosdick87@gmail.com>
    Date:   Wed Aug 28 18:47:15 2024 -0400
    
        Squashed commit of the following:
    
        commit 5c3ab2319287b4729c47d5f8856df56de7cd105b
        Author: ryanontheinstide <ryanfosdick87@gmail.com>
        Date:   Wed Aug 28 18:45:53 2024 -0400
    
            direction amplitude, colorgrade bug fixes
    
        commit ff153b25eb3fcd7dc04b332dc574ba383b14ed19
        Author: ryanontheinstide <ryanfosdick87@gmail.com>
        Date:   Wed Aug 28 17:00:40 2024 -0400
    
            added emission radius
    
        commit 618f63bc2b4bf4b8e5b39e40f96986d19fdc2df6
        Author: ryanontheinstide <ryanfosdick87@gmail.com>
        Date:   Wed Aug 28 15:37:17 2024 -0400
    
            add random flag to particle modulators
    
        commit 67b273dceee1ac289ee16e8c8eb4a6e4b34478e2
        Author: ryanontheinstide <ryanfosdick87@gmail.com>
        Date:   Wed Aug 28 15:26:46 2024 -0400
    
    commit 27cee31feaea720862de1450079b0a344117af28
    Author: ryanontheinstide <ryanfosdick87@gmail.com>
    Date:   Wed Aug 28 13:36:22 2024 -0400
    
        damn version
    
    commit d5883b55290fcc8bc9a45c484500431b97c49d3a
    Author: ryanontheinstide <ryanfosdick87@gmail.com>
    Date:   Wed Aug 28 13:32:49 2024 -0400
    
        Squashed commit of the following:
    
        more feature modulators and flex mask targets
    
    commit e0603a88f971b7de05a1404d9320702d76bb5d3a
    Author: ryanontheinstide <ryanfosdick87@gmail.com>
    Date:   Tue Aug 27 19:20:05 2024 -0400
    
        version update
    
    commit ea2ca47a82a9f5a2b8bdb3b88340e9cd369754c7
    Author: ryanontheinstide <ryanfosdick87@gmail.com>
    Date:   Tue Aug 27 19:18:27 2024 -0400
    
        get torch device
    
    commit 0f9b32e13ea0c6b31ae3dda6b653eb653bf85e22
    Author: ryanontheinstide <ryanfosdick87@gmail.com>
    Date:   Tue Aug 27 18:41:06 2024 -0400
    
        bugfix
    
    commit 8cc1a292f3d185c2ad25f9349c4e2b57cc96d0d0
    Author: ryanontheinstide <ryanfosdick87@gmail.com>
    Date:   Tue Aug 27 18:37:42 2024 -0400
    
        bugfix
    
    commit 7de6853a286dd4d16ae4f29f78cc77005849e2f1
    Author: ryanontheinstide <ryanfosdick87@gmail.com>
    Date:   Tue Aug 27 17:58:23 2024 -0400
    
        Added flex opacity and voronoi scheduled
    
    commit f435740018fbb1b8bbfb274ca17eacd8c5f1a18c
    Author: ryanontheinstide <ryanfosdick87@gmail.com>
    Date:   Tue Aug 27 06:35:15 2024 -0400
    
        bugfix - arbitrary video audio lengths
    
    commit 607703988a05a73b37ee4bd86cb5b0a9daec881a
    Author: ryanontheinstide <ryanfosdick87@gmail.com>
    Date:   Mon Aug 26 10:32:13 2024 -0400
    
        Added flex, audio, midi, help system
    
    commit e239c5a19d1169375e9dc2f72fc03ab2a3143ac4
    Author: ryanontheinstide <ryanfosdick87@gmail.com>
    Date:   Sat Aug 17 18:00:05 2024 -0400
    
        version, temporal control for particle size,color,speed

--- nodes/masks/flex_masks.py
+++ nodes/masks/flex_masks.py
@@ -141,8 +141,8 @@ class FlexMaskVoronoiScheduled(FlexMaskBase):
                 "y_offset": ("FLOAT", {"default": 0.0, "min": -1000.0, "max": 1000.0, "step": 0.1}),
                 "feature_param": (["scale", "detail", "randomness", "seed", "x_offset", "y_offset"],),
                 "formula": (list(cls.formulas.keys()),),
-                "x_offset": ("FLOAT", {"default": 0.0, "min": -1.0, "max": 1.0, "step": 0.01}),
-                "y_offset": ("FLOAT", {"default": 0.0, "min": -1.0, "max": 1.0, "step": 0.01}),
+                "a": ("FLOAT", {"default": 1.0, "min": 0.1, "max": 10.0, "step": 0.1}),
+                "b": ("FLOAT", {"default": 1.0, "min": 0.1, "max": 10.0, "step": 0.1}),
             }
         }
 
